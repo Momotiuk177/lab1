b66f98b8bac59428c988f26099c2764259d32c8f
Momotiuk177 <128076013+Momotiuk177@users.noreply.github.com>
2023-03-21 12:18:36 +0200

    solve

---
+++ src/gdk.java
@@ -0,0 +1,8 @@
+public class gdk {
+    public static void main(String[]args) {
+        double a =2.65;
+        double b =5.49;
+        System.out.println((int)(a+0.5));
+        System.out.println((int)(b+0.5));
+    }
+}
---
+++ src/laba2.java
@@ -0,0 +1,6 @@
+public class laba2 {
+    public static void main(String[]args) {
+        int num =25;
+        System.out.println("Сумма цифр числа: "+(num/10+num%10));
+    }
+}
